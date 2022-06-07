ac54d6b67f48c75aef7d7b29cfcd48725d66ac0b
harvylopz william.lopez.194@gmail.com <william.lopez.194@gmail.com>
2022-06-06 22:41:38 -0600

    update animation leds top

--- pokedex.js
+++ pokedex.js
@@ -55,16 +55,23 @@ function clean(){
 
 
 function userSelect(){
-	
-	document.getElementById("pokemon-info").innerHTML = "";
-	document.getElementById("pokemon-pic-front").innerHTML = "";
-	document.getElementById("pokemon-pic-back").innerHTML = "";
 	let numberPokemon = prompt("Please enter number pokemon");
-	if(numberPokemon > 0 && numberPokemon < 851){
-		writeInfo(numberPokemon);
-	} else {
-		randomNumber();
-	}
+	animationTopLeds();
+
+	//retrasar la ejecucion de la funcion
+	setTimeout(function(){
+		document.getElementById("pokemon-info").innerHTML = "";
+		document.getElementById("pokemon-pic-front").innerHTML = "";
+		document.getElementById("pokemon-pic-back").innerHTML = "";
+		
+		if(numberPokemon > 0 && numberPokemon < 851){
+			writeInfo(numberPokemon);
+		} else {
+			randomNumber();
+		}
+	}, 1000);
+	
+	
 }
 
 function insertText(text, divSelection){
@@ -93,6 +100,52 @@ function animationButtonSelectPokemon(){
 		}, 1500);
 	}	
 }
+
+
+        function changeColor01(){
+            var button_maquina01 = document.getElementById('ball01');
+            button_maquina01.style.backgroundColor = 'rgb(255, 89, 89)';
+            setTimeout(function(){
+            button_maquina01.style.backgroundColor = 'red';
+            }, 100);
+        }
+        function changeColor02(){
+            var button_maquina02 = document.getElementById('ball02');
+            button_maquina02.style.backgroundColor = 'rgb(250, 250, 139)';
+            setTimeout(function(){
+            button_maquina02.style.backgroundColor = 'yellow';
+            }, 100);
+        }
+        function changeColor03(){
+            var button_maquina03 = document.getElementById('ball03');
+            button_maquina03.style.backgroundColor = 'rgb(78, 245, 78)';
+            setTimeout(function(){
+            button_maquina03.style.backgroundColor = 'green';
+            }, 100);
+        }
+
+        function animationTopLeds(){
+            var movents = 0;
+            interval = setInterval(() => {
+                var randomMovent = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+                randomMovent=parseInt(randomMovent);
+                if (randomMovent == 1){
+                    changeColor01();
+                } else if (randomMovent == 2){
+                    changeColor02();
+                } else if (randomMovent == 3){
+                    changeColor03();
+                }
+                
+                movents = movents + 1
+                
+
+                if (movents >= 10) {
+                    clearInterval(interval);
+                }
+            }, 100);
+            
+            }
 
 animationButtonSelectPokemon();
 
